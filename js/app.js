e7983734ddb92bc8059c43ad3266e573d2befaa9
Robin <robinkennedy4@gmail.com>
2018-06-10 23:11:31 +0200

    list of icons added to javascript'
    q

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var listCards = ['fa fa-diamond','fa fa-diamond', 'fa fa-paper-plane-o',
+'fa fa-paper-plane-o','fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt',
+'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle',
+'fa fa-bomb','fa fa-bomb',
+]
 
 /*
  * Display the cards on the page
